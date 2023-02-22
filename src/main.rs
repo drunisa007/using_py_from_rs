bf7638312f5ce9dbd12a61d90c9c6ad6df3eb36e
drunisa007 <newarunkatwal@gmail.com>
2023-02-22 15:36:37 +0630

    call0 is without args and call1 with args

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,51 @@
 use pyo3::prelude::*;
-use pyo3::types::IntoPyDict;
+//use pyo3::types::IntoPyDict;
+use pyo3::types::PyTuple;
 
-fn main() -> PyResult<()>{
+fn main() -> PyResult<()> {
+    // Python::with_gil(|py| {
+    //     // let sys = py.import("sys")?;
+
+    //     // let version: String = sys.getattr("version")?.extract()?;
+
+    //     // let locals = [("os", py.import("os")?)].into_py_dict(py);
+    //     // let code = "os.getenv('USER') or os.getenv('USERNAME') or 'Unknown'";
+    //     // let user: String = py.eval(code, None, Some(&locals))?.extract()?;
+
+    //     // println!("Hello {}, I'm Python {}", user, version);
+    //     // Ok(())
+    // })
+
+    let arg1 = "arg1";
+    let arg2 = "arg2";
+    let arg3 = "arg3";
+
     Python::with_gil(|py| {
-        let sys = py.import("sys")?;
-    
-        let version: String = sys.getattr("version")?.extract()?;
+        let fun: Py<PyAny> = PyModule::from_code(
+            py,
+            "def example(*args, **kwargs):
+                if args != ():
+                    print('called with args', args)
+                if kwargs != {}:
+                    print('called with kwargs', kwargs)
+                if args == () and kwargs == {}:
+                    print('called with no arguments')",
+            "",
+            "",
+        )?
+        .getattr("example")?
+        .into();
 
-        let locals = [("os", py.import("os")?)].into_py_dict(py);
-        let code = "os.getenv('USER') or os.getenv('USERNAME') or 'Unknown'";
-        let user: String = py.eval(code, None, Some(&locals))?.extract()?;
+        // call object without any arguments
+        fun.call0(py)?;
 
-        println!("Hello {}, I'm Python {}", user, version);
+        // call object with PyTuple
+        let args = PyTuple::new(py, &[arg1, arg2, arg3]);
+        fun.call1(py, args)?;
+
+        // pass arguments as rust tuple
+        let args = (arg1, arg2, arg3);
+        fun.call1(py, args)?;
         Ok(())
     })
 }
