aeea0e94f296b0c83f105a2ef08f2131199203df
drunisa007 <newarunkatwal@gmail.com>
2023-03-01 11:54:39 +0630

    testing with pyo3 to load single file executable

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,15 @@
-use std::path::{PathBuf};
-
-use pyembed::OxidizedPythonInterpreterConfig;
-
-
 fn main() {
+    // let config = OxidizedPythonInterpreterConfig::default();
+    
+    // // let current_path =  Path::new("/Users/drunisa/python_test/using_py_from_rs");
+    // // config.exe = Some(current_path.to_owned());
+    // // println!("{:?}",config.exe);
+    // pyembed::
 
-    let config = OxidizedPythonInterpreterConfig {
-        exe: Some(PathBuf::from("/Users/drunisa/python_test/using_py_from_rs")),
-        ..Default::default()
-    };
-    // let current_path =  Path::new("/Users/drunisa/python_test/using_py_from_rs");
-    // config.exe = Some(current_path.to_owned());
-    // println!("{:?}",config.exe);
+    // let interpreter = pyembed::MainPythonInterpreter::new(config).unwrap();
 
-   let interpreter =  pyembed::MainPythonInterpreter::new(config).unwrap();
-
-   interpreter.with_gil(|py| {
-    match py.eval("print('hello, world')",None,None) {
-       Ok(_) => print!("python code executed successfully"),
-       Err(e) => print!("python error: {:?}", e),
-   }
-});
+    // interpreter.with_gil(|py| match py.eval("print('hello, world')", None, None) {
+    //     Ok(_) => print!("python code executed successfully"),
+    //     Err(e) => print!("python error: {:?}", e),
+    // });
 }
