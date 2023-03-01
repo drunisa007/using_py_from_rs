ad5f19a0ae4129d2e2d5db3864e7e0e33e371986
drunisa007 <newarunkatwal@gmail.com>
2023-03-02 00:15:07 +0630

    fail build

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,25 @@
-use pyo3::{PyResult, Python, types::PyList};
+//use std::{env, path::PathBuf};
 
-fn main(){
+use std::path::Path;
+
+use pyembed::OxidizedPythonInterpreterConfig;
+
+//use pyo3::{PyResult, Python, types::PyList};
+fn main() {
+
+    let mut config = OxidizedPythonInterpreterConfig::default();
+
+    let current_path =  Path::new("/Users/drunisa/python_test/using_py_from_rs");
+    config.exe = Some(current_path.to_owned());
+    println!("{:?}",config.exe);
+
+    let interpreter = pyembed::MainPythonInterpreter::new(config).unwrap();
+
+    // interpreter.with_gil(|py| match py.eval("print('hello, world')", None, None) {
+    //     Ok(_) => print!("python code executed successfully"),
+    //     Err(e) => print!("python error: {:?}", e),
+    // });
+
     // Python::with_gil(|py| {
     //     let sys = py.import("sys")?;
     //     let path:&PyList = sys.getattr("path")?.extract()?;
@@ -12,17 +31,5 @@ fn main(){
     //     println!("{:?}",result.call0());
     //     Ok(())
     // })
-    // let config = OxidizedPythonInterpreterConfig::default();
-
-    // // let current_path =  Path::new("/Users/drunisa/python_test/using_py_from_rs");
-    // // config.exe = Some(current_path.to_owned());
-    // // println!("{:?}",config.exe);
-    // pyembed::
-
-    // let interpreter = pyembed::MainPythonInterpreter::new(config).unwrap();
-
-    // interpreter.with_gil(|py| match py.eval("print('hello, world')", None, None) {
-    //     Ok(_) => print!("python code executed successfully"),
-    //     Err(e) => print!("python error: {:?}", e),
-    // });
+ 
 }
